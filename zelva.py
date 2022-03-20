dbb444ce958f0f9954b2404d95296eade6505d68
tomikko <101982220+tomikko@users.noreply.github.com>
2022-03-20 17:35:10 +0100

    Update zelva.py

--- zelva.py
+++ zelva.py
@@ -13,12 +13,12 @@ circle = turtle.Turtle()
 circle.shape("circle")
 circle.pencolor("green")
 
+circle.left(90)
 circle.penup()
 circle.goto(300, -300)
 
 triangle.goto(-300, 200)
 triangle.pendown()
-
 triangle.right(90)
 triangle.forward(25)
 
@@ -33,47 +33,27 @@ triangle.penup()
 triangle.left(90)
 triangle.goto(-300, -300)
 
-circle.goto(300, 225)
+circle.goto(300, 200)
+circle.right(90)
 circle.pendown()
 circle.circle(25)
+
 circle.penup()
 circle.left(90)
-circle.forward(25)
-circle.right(90)
-circle.goto(275, -300)
+circle.goto(300, -300)
 
-print("Begin!\n\n")
+print("Begin!")
 die = [1, 2, 3, 4, 5, 6]
 
-for i in range(20):
-    if triangle.pos() >= (-300, 200):
+while True:
+    tri_y = triangle.ycor()
+    cir_y = circle.ycor()
+
+    if tri_y >= 200:
         print("\nTriangle wins!")
-        break
 
-    elif circle.pos() >= (300, 225):
-        print("\nCircle wins\n")
-        break
-
-    else:
-        triangle_turn = input("\nPress Enter to roll the die\n")
-        die_outcome = random.choice(die)
-        print("The result of the die roll is: ")
-        print(die_outcome)
-        print("The number of steps will be: ")
-        print(20 * die_outcome)
-        triangle.forward(20 * die_outcome)
-
-        circle_turn = input("Press Enter to roll the die")
-
-        die_outcome = random.choice(die)
-        print("The result of the die roll is: ")
-        print(die_outcome)
-        print("The number of steps will be: ")
-        print(20 * die_outcome)
-        circle.fd(20 * die_outcome)
-
-    if triangle.pos() >= (-300, 200):
         triangle.fillcolor("yellow")
+        triangle.pendown()
         triangle.begin_fill()
         triangle.right(90)
         triangle.forward(25)
@@ -85,9 +65,44 @@ for i in range(20):
         triangle.left(120)
         triangle.forward(25)
         triangle.end_fill()
+        break
 
-    elif circle.pos() >= (300, 225):
+    if cir_y >= 200:
+        print("\nCircle wins\n")
+
+        circle.right(90)
+
         circle.fillcolor("yellow")
+        circle.pendown()
         circle.begin_fill()
         circle.circle(25)
         circle.end_fill()
+        break
+
+    print("\n\nTriangle's turn:")
+    input("Press Enter to roll the die\n")
+
+    die_outcome = random.choice(die)
+    tri_steps = 20 * die_outcome
+    tri_num = (tri_y + tri_steps) - 200
+    tri_neumimvymysletnazvyprostepresah = (tri_num / abs(tri_num) + 1) / 2 * tri_num
+
+    print("The result of the die roll is: ")
+    print(die_outcome)
+    print("\nThe number of steps will be:")
+    print(tri_steps)
+    triangle.forward(tri_steps - tri_neumimvymysletnazvyprostepresah)
+
+    print("\nCircle's turn:")
+    input("Press Enter to roll the die\n")
+
+    die_outcome = random.choice(die)
+    cir_steps = 20 * die_outcome
+    cir_num = (cir_y + cir_steps) - 200
+    cir_neumimvymysletnazvyprostepresah = (cir_num / abs(cir_num) + 1) / 2 * cir_num
+
+    print("The result of the die roll is: ")
+    print(die_outcome)
+    print("\nThe number of steps will be: ")
+    print(cir_steps)
+    circle.forward(cir_steps - cir_neumimvymysletnazvyprostepresah)

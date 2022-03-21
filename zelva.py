d10ddac7a029cebae628234ee05b0268e8c54e38
tomikko <101982220+tomikko@users.noreply.github.com>
2022-03-21 18:18:31 +0100

    Update zelva.py

--- zelva.py
+++ zelva.py
@@ -1,30 +1,35 @@
 import turtle
 import random
+
 triangle = turtle.Turtle()
 triangle.shape("triangle")
-triangle.pen(pencolor = "brown", fillcolor = "brown")
+triangle.pen(pencolor="brown", fillcolor="brown")
 triangle.lt(90)
 triangle.pu()
 triangle.goto(-300, -300)
-circle = triangle.clone()
+
+circle = turtle.Turtle()
 circle.shape("circle")
-circle.pen(pencolor = "green", fillcolor = "green")
+circle.pen(pencolor="green", fillcolor="green")
+circle.lt(90)
 circle.pu()
 circle.goto(300, -300)
-triangle.goto(-300, 200)
 
+triangle.goto(-300, 200)
 triangle.pd()
 triangle.rt(90)
 triangle.fd(25)
-for i in 1,2:
+
+for i in range(2):
     triangle.lt(120)
     triangle.fd(50)
+
 triangle.lt(120)
 triangle.fd(25)
 triangle.pu()
-
 triangle.lt(90)
 triangle.goto(-300, -300)
+
 circle.goto(300, 225)
 circle.pd()
 circle.circle(25)
@@ -33,50 +38,64 @@ circle.lt(90)
 circle.fd(25)
 circle.rt(90)
 circle.goto(275, -300)
+
 print("Begin!")
-die = [1,2,3,4,5,6]
+die = [1, 2, 3, 4, 5, 6]
 
 while True:
     if triangle.ycor() >= 200:
         print("Triangle wins!")
+
         if triangle.ycor() > 200:
             triangle.setpos(-300, 200)
-        triangle.pd
+
+        triangle.pd()
         triangle.begin_fill()
         triangle.rt(90)
         triangle.fd(25)
-        for i in 1,2:
+
+        for i in range(2):
             triangle.lt(120)
             triangle.fd(50)
+
         triangle.lt(120)
         triangle.fd(25)
-        triangle.end_fill()
         triangle.lt(90)
+
+        triangle.end_fill()
         triangle.hideturtle()
         break
+
     if circle.ycor() >= 200:
         print("Circle wins")
+
         if circle.ycor() > 200:
             circle.setpos(300, 225)
-        circle.pd
+
+        circle.pd()
         circle.begin_fill()
         circle.circle(25)
+
         circle.end_fill()
         circle.hideturtle()
         break
-print("Triangle's turn: ")
+
+    print("Triangle's turn: ")
     input("Press Enter to roll the die")
     die_outcome = random.choice(die)
+
     print("The result of the die roll is: ")
     print(die_outcome)
     print("The number of steps will be: ")
-    print(20die_outcome)
-    triangle.fd(20die_outcome)
+    print(20 * die_outcome)
+    triangle.fd(20 * die_outcome)
+
     print("Circle's turn: ")
     input("Press Enter to roll the die")
     die_outcome = random.choice(die)
+
     print("The result of the die roll is: ")
     print(die_outcome)
     print("The number of steps will be: ")
-    print(20die_outcome)
-    circle.fd(20die_outcome)
+    print(20 * die_outcome)
+    circle.fd(20 * die_outcome)

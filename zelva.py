7d75463b3e933b2ab6beffbb989df488241d84e9
tomikko <101982220+tomikko@users.noreply.github.com>
2022-03-25 20:26:04 +0100

    Update zelva.py

--- zelva.py
+++ zelva.py
@@ -1,17 +1,18 @@
 import turtle
 import random
-t = turtle.Turtle()
-screen = turtle.getscreen()
-t.speed(30)
 
+
+"""
 sirka = 30
 vyska = 100
-def kniha(sirka,vyska):
+def kniha(sirka, vyska):
     for i in range(2):
         t.fd(sirka)
         t.lt(90)
         t.fd(vyska)
         t.lt(90)
+"""
+
 
 def shelf():
     for i in range(2):
@@ -20,8 +21,36 @@ def shelf():
         t.fd(100)
         t.lt(90)
 
+
 def line():
     t.fd(600)
+
+
+def book():
+    width = random.randint(min_width, max_width)
+    height = random.randint(min_height, max_height)
+
+    for i in range(2):
+        t.fd(width)
+        t.lt(90)
+        t.fd(height)
+        t.lt(90)
+
+    t.fd(width)
+
+
+def color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+
+    t.fillcolor(r, g, b)
+
+
+t = turtle.Turtle()
+screen = turtle.Screen()
+screen.colormode(255)
+t.speed(30)
 
 t.pu()
 t.goto(-200, -300)
@@ -42,37 +71,17 @@ for i in range(4):
     t.lt(90)
     t.fd(100)
 
+t.rt(90)
+t.fd(10)
+t.speed(0)
+
 min_width = 20
 max_width = 30
 min_height = 60
 max_height = 80
 
-
-def book():
-    width = random.randint(min_width, max_width)
-    height = random.randint(min_height, max_height)
-    for i in range(2):
-        t.fd(width)
-        t.lt(90)
-        t.fd(height)
-        t.lt(90)
-
-    t.fd(width)
-
-t.rt(90)
-t.fd(10)
-t.speed(0)
-
-t.screen.colormode(255)
-def color():
-    R = random.randint(0, 255)
-    B = random.randint(0, 255)
-    G = random.randint(0, 255)
-
-    turtle.fillcolor(R, G, B)
-
 for i in range(10):
-    t.begin_fill()
     color()
+    t.begin_fill()
     book()
     t.end_fill()
